0def0ef0310f4aec7fef42fde5476528abc3600a
Simone <simone.pagotto.kg@gmail.com>
2021-02-02 16:07:25 +0100

    CategoryFactory
    -slug mantenuto human readable

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -6,9 +6,11 @@ use App\Category;
 use Faker\Generator as Faker;
 
 $factory->define(App\Category::class, function (Faker $faker) {
-    $fakeSentence = rtrim($faker->sentence($nbWords = random_int(1,5), $variableNbWords = true),".");
+    //genero con faker la mia frase che utilizzerò per title e slug
+    //rimuovo tutti i caratteri non alfanumerici e taglio il punto finale
+    $fakeSentence = preg_replace("/[^A-Za-z0-9 ]/",'',rtrim($faker->sentence($nbWords = random_int(1,5), $variableNbWords = true),"."));
     return [
         'title' => $fakeSentence,
-        'slug' => str_replace(' ','-',$fakeSentence),
+        'slug' => str_replace(' ','-',$fakeSentence),//sostituisco gli spazi bianchi con i trattini per lasciare lo slug human readable
     ];
 });
